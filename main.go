d64ae31f55bfacf704498ade976fada71191936d
wjs1152283574 <44918318+wjs1152283574@users.noreply.github.com>
2021-03-20 10:27:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	defer appmysql.DB.Close()                               // 预操作: 关闭数据库连接
+	defer appmysql.DB.Close()                               // 预操作: 关闭 数据库连接
 	defer timer.Conrs.Stop()                                // 预操作: 关闭定时器任务
 	routers.InitAPIRouter().Run(parsecfg.GlobalConfig.Port) // 在 main中阻塞监听
 }
