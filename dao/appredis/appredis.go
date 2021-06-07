07691eae081b3740bcfd29e4196d71643754fedd
“Casso” <1152283574@qq.com>
2021-06-07 11:58:32 +0800

    fead(mysql.go):add test env

--- dao/appredis/appredis.go
+++ dao/appredis/appredis.go
@@ -1,31 +1,54 @@
+/*
+ * @Author: Casso-Wong
+ * @Date: 2021-06-04 14:41:32
+ * @Last Modified by:   Casso-Wong
+ * @Last Modified time: 2021-06-04 14:41:32
+ */
 package appredis
 
 import (
 	"encoding/json"
-	"fmt"
 	"goweb/utils/parsecfg"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
 
+// redis 数据库初始化 以及具体配置
+
 // RedisDefaultPool redis 连接池
 var RedisDefaultPool *redis.Pool
 
 func init() {
-	RedisDefaultPool = NewPool(parsecfg.GlobalConfig.Redis.Host, parsecfg.GlobalConfig.Redis.Port, parsecfg.GlobalConfig.Redis.MaxIdle, parsecfg.GlobalConfig.Redis.MaxActive)
+	if parsecfg.GlobalConfig.Env == "dev" {
+		RedisDefaultPool = NewPool(parsecfg.GlobalConfig.Redis.Host, parsecfg.GlobalConfig.Redis.Port, parsecfg.GlobalConfig.Redis.Auth, parsecfg.GlobalConfig.Redis.MaxIdle, parsecfg.GlobalConfig.Redis.MaxActive)
+	}
+	if parsecfg.GlobalConfig.Env == "test" {
+		RedisDefaultPool = NewPool(parsecfg.GlobalConfig.Redis.HostLive, parsecfg.GlobalConfig.Redis.PortLive, parsecfg.GlobalConfig.Redis.Auth, parsecfg.GlobalConfig.Redis.MaxIdle, parsecfg.GlobalConfig.Redis.MaxActive)
+	}
 }
 
 // NewPool 项目运行初始化redis连接池
-func NewPool(addr, port string, max, maxactive int) *redis.Pool { // 传入 ip:port 最大闲置连接数 最大活跃连接数
+func NewPool(addr, port, auth string, max, maxactive int) *redis.Pool { // 传入 ip:port 最大闲置连接数 最大活跃连接数
 	str := addr + ":" + port
-	fmt.Println(str)
 	return &redis.Pool{
 		MaxIdle:     max,
 		MaxActive:   maxactive,
 		IdleTimeout: 240 * time.Second,
-		Dial: func() (conn redis.Conn, e error) {
-			return redis.Dial("tcp", str)
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", str)
+			if err != nil {
+				return nil, err
+			}
+			if _, err := c.Do("AUTH", auth); err != nil {
+				c.Close()
+				return nil, err
+			}
+			if _, err := c.Do("SELECT", 0); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, nil
 		},
 	}
 }
@@ -53,13 +76,20 @@ func SetJSON(key string, data interface{}, time int) error {
 	return nil
 }
 
-// SetArr 列表插入
-func SetArr(key, val string) error {
+// SetExpire 是这基础类型+ TTL
+func SetExpire(key string, data interface{}, time int) error {
 	conn := RedisDefaultPool.Get()
 	defer conn.Close()
+	_, err := conn.Do("SET", key, data)
+	if err != nil {
+		return err
+	}
 
-	_, err := conn.Do("SADD", key, val)
-	return err
+	_, err = conn.Do("EXPIRE", key, time)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // SetHash 设置基础类型
@@ -68,6 +98,23 @@ func SetHash(key, val string) error {
 	defer conn.Close()
 
 	_, err := conn.Do("set", key, val)
+	return err
+}
+
+// SetHash 设置基础类型
+func SetArr(key, val string) error {
+	conn := RedisDefaultPool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("SADD", key, val)
+	return err
+}
+
+// DelArr 移除list指定值
+func DelArr(key, val string) error {
+	conn := RedisDefaultPool.Get()
+	defer conn.Close()
+	_, err := conn.Do("LREM", key, 0, val)
 	return err
 }
 
@@ -103,6 +150,22 @@ func Delete(key string) (bool, error) {
 	return redis.Bool(conn.Do("DEL", key))
 }
 
+// GetLIke 获取相似Key
+func GetLIke(key string) (res [][]byte, err error) {
+	conn := RedisDefaultPool.Get()
+	defer conn.Close()
+	keys, err := redis.Strings(conn.Do("KEYS", "*"+key+"*"))
+	if err != nil {
+		return [][]byte{}, err
+	}
+	var r [][]byte
+	for _, v := range keys {
+		val, _ := Get(v)
+		r = append(r, val)
+	}
+	return r, nil
+}
+
 // LikeDeletes 删除相似key
 func LikeDeletes(key string) error {
 	conn := RedisDefaultPool.Get()
@@ -129,6 +192,28 @@ func Rpop(key string) ([]byte, error) {
 	defer conn.Close()
 
 	reply, err := redis.Bytes(conn.Do("rpop", key))
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+// Rpush 插入列表尾部
+func Rpush(key string, val []byte) ([]byte, error) {
+	conn := RedisDefaultPool.Get()
+	defer conn.Close()
+	reply, err := redis.Bytes(conn.Do("RPUSH", key, val))
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
+}
+
+// GetList 获取列表
+func GetList(key string) (res []string, err error) {
+	conn := RedisDefaultPool.Get()
+	defer conn.Close()
+	reply, err := redis.Strings(conn.Do("LRANGE", key, 0, -1))
 	if err != nil {
 		return nil, err
 	}
