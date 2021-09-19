d03faa82db8426145cb042841d0bda963796ae82
Casso <1152283574@qq.com>
2021-09-19 15:44:32 +0800

    docs(README):更新文档

--- utils/datacopy/datacopy.go
+++ utils/datacopy/datacopy.go
@@ -2,7 +2,7 @@
  * @Author: Casso-Wong
  * @Date: 2021-09-09 14:18:34
  * @Last Modified by: Casso-Wong
- * @Last Modified time: 2021-09-18 19:31:39
+ * @Last Modified time: 2021-09-19 15:44:29
  * 数据复制，专门用于请求体数据映射到DTO模型。
  * 原因：ShouldBind时需要限制字段，如果直接使用DTO模型接受请求数据则不好限制字段；
  * 又不希望每次都每个字段都显示赋值，所以封装一个基于json包的数据复制功能
@@ -28,7 +28,7 @@ func DataReflact(src, dst *reflect.Value) *reflect.Value {
 	for i := 0; i < src.NumField(); i++ {
 		filed := src.Type().Field(i).Name
 		cfiled := dst.FieldByName(filed)
-		if cfiled.IsValid() && cfiled.CanSet() { // 可用的属性且可赋值
+		if cfiled.IsValid() && cfiled.CanSet() { // 可用的属性且可 赋值
 			dst.Field(i).Set(src.Field(i))
 		}
 	}
