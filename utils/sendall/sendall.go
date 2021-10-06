b99ce1a85a3eae732dbbf28bb44b8847881166ce
Casso <1152283574@qq.com>
2021-10-06 20:10:39 +0800

    fix(util/sendall):remove

--- utils/sendall/sendall.go
+++ utils/sendall/sendall.go
@@ -6,23 +6,17 @@
  */
 package sendall
 
-import (
-	dto "goweb/model/dto/user"
-	vo "goweb/model/vo/user"
-	"goweb/utils/ws"
-)
-
 // 统一发送给全部用户，后期可能会根据传入app_code 区别对待用户，所以放在这里统一处理
 
 // NotifyAll 通知所有移动端用户更新配置
-func NotifyAll(code int, codemsg string) {
-	var u dto.User
-	var res []vo.GetSendAll
-	u.GetFrontU(&res)
-	var msg ws.TranstMsg
-	msg.MsgType = int64(code)
-	msg.Msg = codemsg
-	for _, v := range res {
-		ws.CheckInlineOutlingSend([]string{v.Mobile}, msg, false)
-	}
-}
+// func NotifyAll(code int, codemsg string) {
+// 	var u dto.User
+// 	var res []vo.GetSendAll
+// 	u.GetFrontU(&res)
+// 	var msg ws.TranstMsg
+// 	msg.MsgType = int64(code)
+// 	msg.Msg = codemsg
+// 	for _, v := range res {
+// 		ws.CheckInlineOutlingSend([]string{v.Mobile}, msg, false)
+// 	}
+// }
